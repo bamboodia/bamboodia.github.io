840ec1bb018bdaf9ff171e13a974114d28566c9a
bamboodia <crum82@gmail.com>
2021-10-11 18:23:29 +0100

    First Iteration Of SpotOn

---
+++ spoton/src/Components/App/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>Spot<span className="highlight">On</span></h1>
+      <div className="App">
+        <SearchBar />
+        <div className="App-playlist">
+          <SearchResults />
+          <Playlist />
+        </div>
+      </div>
+    </div>
+    )
+  }
+}
+
+export default App;
