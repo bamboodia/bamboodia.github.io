7a7c7afc9f957aa7bea1aa68b777cf397107e308
bamboodia <crum82@gmail.com>
2021-10-18 00:36:45 +0100

    SpotOn

--- spoton/src/Components/Playlist/Playlist.js
+++ spoton/src/Components/Playlist/Playlist.js
@@ -1,14 +1,23 @@
 import React from "react"
-
 import "./Playlist.css"
+import TrackList from '../Tracklist/Tracklist'
 
 class Playlist extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleNameChange = this.handleNameChange.bind(this)
+	}
+	handleNameChange(e) {
+		this.props.onNameChange(e.target.value)
+	}
 	render() {
 		return (
 			<div className="Playlist">
-				<input defaultValue={'New Playlist'} />
-				{/* <TrackList /> */}
-				<button className="Playlist-save">SAVE TO SPOTIFY</button>
+				<input defaultValue={'Playlist Name'} onChange={this.handleNameChange} />
+				<TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} onPlay={this.props.onPlay}
+					onStop={this.props.onStop} 
+					isPlaying={this.props.isPlaying}/>
+				<button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
 			</div>
 		)
 	}
